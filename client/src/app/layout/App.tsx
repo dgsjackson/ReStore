5f1bb94afe1aed5139f7e39b2b9b62628ed07f4a
Dylan Jackson <dylangsjackson@gmail.com>
2023-09-24 10:09:06 -0500

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline, Switch, ThemeProvider, createTheme } from "@mui/material";
+import Header from "./Header";
+import { useState } from "react";
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const paletteType = darkMode ? 'dark': 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}></Header>
+      <Container>
+        <Catalog></Catalog>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
